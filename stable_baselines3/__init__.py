b3eeb89cc7dd7c01ffe3d1c3f698b5640180c061
Yann Berthelot <yannberthelot1@gmail.com>
2024-07-10 17:22:44 +0200

    added avec PPO and its corrected version

--- stable_baselines3/__init__.py
+++ stable_baselines3/__init__.py
@@ -6,6 +6,7 @@ from stable_baselines3.ddpg import DDPG
 from stable_baselines3.dqn import DQN
 from stable_baselines3.her.her_replay_buffer import HerReplayBuffer
 from stable_baselines3.ppo import PPO
+from stable_baselines3.avec_ppo import AVEC_PPO, CORRECTED_AVEC_PPO
 from stable_baselines3.sac import SAC
 from stable_baselines3.td3 import TD3
 
@@ -27,6 +28,8 @@ __all__ = [
     "DDPG",
     "DQN",
     "PPO",
+    "AVEC_PPO",
+    "CORRECTED_AVEC_PPO",
     "SAC",
     "TD3",
     "HerReplayBuffer",
