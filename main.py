591393fc9202f3a5a9d0f7fae12164b34efc495b
Yann Berthelot <yannberthelot1@gmail.com>
2024-07-11 11:51:31 +0200

    updated exp script for batch sizes

--- main.py
+++ main.py
@@ -10,23 +10,33 @@ if __name__ == "__main__":
     seed = int(sys.argv[1])
     env_name = str(sys.argv[2])
     mode = str(sys.argv[3])
+
+    batch_sizes = [32, 64, 128, 256]
+    n_timesteps = int(1e6)
+    num_envs = 4
+
+    agents_dict = {"AVEC": AVEC_PPO, "CORRECTED_AVEC": CORRECTED_AVEC_PPO, "PPO": PPO}
+
     set_random_seed(seed)
-    run = wandb.init(
-        # Set the project where this run will be logged
-        project="avec experiments",
-        # Track hyperparameters and run metadata
-        sync_tensorboard=True,
-        config={
-            "mode": mode,
-            "env": env_name,
-        },
-    )
-    env = make_vec_env(env_name, n_envs=4)
-    if mode == "AVEC":
-        model = AVEC_PPO("MlpPolicy", env, verbose=1, tensorboard_log=f"runs/{run.id}")
-    elif mode == "CORRECTED_AVEC":
-        model = CORRECTED_AVEC_PPO("MlpPolicy", env, verbose=1, tensorboard_log=f"runs/{run.id}")
-    else:
-        model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=f"runs/{run.id}")
-    model.learn(total_timesteps=1_000_000, callback=WandbCallback())
-    run.finish()
+
+    for batch_size in batch_sizes:
+        run = wandb.init(
+            # Set the project where this run will be logged
+            project="avec experiments",
+            # Track hyperparameters and run metadata
+            sync_tensorboard=True,
+            config={
+                "agent": "PPO",
+                "mode": mode,
+                "env": env_name,
+                "seed": seed,
+                "batch size": batch_size,
+                "num envs": num_envs,
+                "num timesteps": n_timesteps,
+            },
+        )
+        env = make_vec_env(env_name, n_envs=num_envs)
+        agent = agents_dict[mode]
+        model = agent("MlpPolicy", env, tensorboard_log=f"runs/{run.id}", batch_size=batch_size)
+        model.learn(total_timesteps=n_timesteps, callback=WandbCallback())
+        run.finish()
