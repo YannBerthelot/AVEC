5b937771b6b9227c94b93e0b5269d19cb8b136bb
Yann Berthelot <yannberthelot1@gmail.com>
2024-07-21 22:47:47 +0200

    remove legacy corrected avec ppo from imports

--- stable_baselines3/avec_ppo/__init__.py
+++ stable_baselines3/avec_ppo/__init__.py
@@ -1,4 +1,4 @@
 from stable_baselines3.avec_ppo.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
-from stable_baselines3.avec_ppo.avec_ppo import AVEC_PPO, CORRECTED_AVEC_PPO
+from stable_baselines3.avec_ppo.avec_ppo import AVEC_PPO
 
-__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "AVEC_PPO", "CORRECTED_AVEC_PPO"]
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "AVEC_PPO"]
