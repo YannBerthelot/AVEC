615fc5ebbd28df1ac9e4446b1bab35cd2adf164c
Yann Berthelot <yannberthelot1@gmail.com>
2024-08-14 23:26:22 +0200

    added AVEC-SAC

---
+++ stable_baselines3/avec_sac/__init__.py
@@ -0,0 +1,4 @@
+from stable_baselines3.sac.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+from stable_baselines3.avec_sac.avec_sac import AVEC_SAC
+
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "AVEC_SAC"]
