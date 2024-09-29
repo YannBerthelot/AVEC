fix evaluation of gradients

main.py
@@ -18,6 +18,7 @@ from functools import partial
 
 DEFAULT_N_STEPS = 2048
 DEFAULT_BUFFER_SIZE = 1_000_000
+from dotenv import load_dotenv
 
 
 def read_hyperparams_data(file_name):
@@ -64,6 +65,7 @@ def parse_hyperparams(env_name, hyperparams_data):
 
 
 if __name__ == "__main__":
+
     seed = int(sys.argv[1])
     env_name = str(sys.argv[2])
     mode = str(sys.argv[3])
@@ -75,6 +77,9 @@ if __name__ == "__main__":
     N_EVAL_TIMESTEPS = int(eval(sys.argv[8]))
     N_SAMPLES_MC = int(sys.argv[9])
     N_EVAL_ENVS = int(sys.argv[10])
+
+    load_dotenv(".env")
+    LOCAL = eval(os.environ.get("LOCAL"))
 
     num_threads = int(psutil.cpu_count() / psutil.cpu_count(logical=False))
     torch.set_num_threads(num_threads)
@@ -126,8 +131,9 @@ if __name__ == "__main__":
             )
     agent_name = "PPO" if "PPO" in mode else "SAC"
     number = 7 if "PPO" in mode else 6
+    prefix = " local" if LOCAL else ""
     run = wandb.init(
-        project=f"avec experiments {agent_name} {number}",
+        project=f"avec experiments {agent_name} {number}{prefix}",
         sync_tensorboard=True,
         config={
             "agent": mode,

stable_baselines3/common/avec_utils.py
@@ -2,7 +2,7 @@ from operator import iconcat
 from functools import reduce
 import os
 import wandb
-from typing import Optional, Callable
+from typing import Optional
 import json
 import pickle
 from math import ceil
@@ -23,9 +23,26 @@ from dotenv import load_dotenv
 import boto3
 from botocore.exceptions import ClientError
 import logging
-from paramiko import SSHClient, SSHConfig
-from scp import SCPClient
 from subprocess import Popen
+from stable_baselines3.common import utils
+import pdb
+import numpy as np
+from math import ceil
+
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import VecNormalize
+from stable_baselines3.common.utils import set_random_seed
+import wandb
+import torch
+import torch.nn as nn
+import yaml
+import os
+import psutil
+from copy import deepcopy
+from functools import partial
+import shutil
+import math
+from pathlib import Path
 
 
 def copy_from_host(destination_file: Path, host: str, host_file: Path) -> None:
@@ -42,7 +59,7 @@ def upload_file(file_name, bucket, object_name=None):
     :param object_name: S3 object name. If not specified then file_name is used
     :return: True if file was uploaded, else False
     """
-    load_dotenv("/home/yberthel/AVEC/.env")
+
     aws_access_key_id = os.environ.get("ACCESS_KEY_ID")
     aws_secret_access_key = os.environ.get("SECRET_ACCESS_KEY")
     s3_client = boto3.client("s3", aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
@@ -237,7 +254,7 @@ def compute_true_grads(self, alpha: float) -> list:
     return true_grads
 
 
-def get_true_grads_from_policy(self, alpha: float, env_name: str, num_envs: int = 1, n_steps: int = int(1e5)):
+def get_true_grads_from_policy(self, alpha: float, env_name: str, num_envs: int = 1, n_steps: int = int(1e6)):
     env = make_vec_env(env_id=env_name, n_envs=num_envs)
     self._last_obs = env.reset()
     n_rollout_steps = ceil(n_steps / num_envs)
@@ -252,6 +269,8 @@ def get_true_grads_from_policy(self, alpha: float, env_name: str, num_envs: int 
             n_envs=num_envs,
             **self.rollout_buffer_kwargs,
         )
+        self.rollout_buffer = rollout_buffer
+
         self.collect_rollouts(
             env=env,
             callback=None,
@@ -263,23 +282,21 @@ def get_true_grads_from_policy(self, alpha: float, env_name: str, num_envs: int 
         )
         self.train(update=False, n_epochs=1, alpha=alpha)
     else:
-        rollout_buffer = self.replay_buffer_class(
-            n_rollout_steps,
-            self.observation_space,  # type: ignore[arg-type]
-            self.action_space,
-            device=self.device,
-            n_envs=num_envs,
-            optimize_memory_usage=self.optimize_memory_usage,
-            **self.replay_buffer_kwargs,
-        )
-        import pdb
-
-        pdb.set_trace()
+        # rollout_buffer = self.replay_buffer_class(
+        #     n_rollout_steps,
+        #     self.observation_space,  # type: ignore[arg-type]
+        #     self.action_space,
+        #     device=self.device,
+        #     n_envs=num_envs,
+        #     optimize_memory_usage=self.optimize_memory_usage,
+        #     **self.replay_buffer_kwargs,
+        # )
+        # self.replay_buffer = rollout_buffer
         self.collect_rollouts(
             env=env,
             callback=None,
             train_freq=n_rollout_steps,
-            replay_buffer=rollout_buffer,
+            replay_buffer=self.replay_buffer,
             flag=False,
             update=False,
             value_function_eval=False,
@@ -765,3 +782,246 @@ def get_grad_from_net(net, grads=None) -> list:
     for parameter in net.parameters():
         grads.append(parameter.grad)
     return grads
+
+
+def copy_from_host(destination_file: Path, host: str, host_file: Path) -> None:
+    os.system(f"scp -r {host}:{host_file} {destination_file}")
+
+
+def find_nearest(array, value):
+    idx = np.searchsorted(array, value, side="left")
+    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx - 1]) < math.fabs(value - array[idx])):
+        return array[idx - 1]
+    else:
+        return array[idx]
+
+
+def read_hyperparams_data(file_name):
+    with open(file_name) as stream:
+        try:
+            hyperparams_data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+        return hyperparams_data
+
+
+def linear_schedule(x, init_x):
+    return x * init_x
+
+
+def parse_hyperparams(env_name, hyperparams_data):
+    hyperparams = hyperparams_data[env_name]
+    for key, value in hyperparams.items():
+        if isinstance(value, str):
+            if "lin" in value:
+                true_val = float(value.split("_")[1])
+                hyperparams[key] = partial(linear_schedule, init_x=true_val)
+    if "normalize" in hyperparams.keys():
+        normalize = hyperparams.pop("normalize")
+    else:
+        normalize = False
+    if "n_envs" in hyperparams.keys():
+        n_envs = hyperparams.pop("n_envs")
+    else:
+        n_envs = 1
+    if "policy" in hyperparams.keys():
+        policy = hyperparams.pop("policy")
+    else:
+        policy = "MlpPolicy"
+    if "n_timesteps" in hyperparams.keys():
+        n_timesteps = hyperparams.pop("n_timesteps")
+    else:
+        n_timesteps = None
+    if "policy_kwargs" in hyperparams.keys():
+        if isinstance(hyperparams["policy_kwargs"], str):
+            hyperparams["policy_kwargs"] = eval(hyperparams["policy_kwargs"])
+            assert isinstance(hyperparams["policy_kwargs"], dict)
+    return n_envs, policy, hyperparams, normalize, n_timesteps
+
+
+def set_model(
+    seed,
+    env_name,
+    mode,
+    flag,
+    alpha,
+    n_steps_factor,
+    network_size_factor,
+    n_eval_timesteps,
+    n_eval_envs,
+    n_samples_MC,
+    run,
+    yaml_path,
+    n_timesteps_user=None,
+):
+    from stable_baselines3 import AVEC_PPO, PPO, AVEC_SAC, SAC
+
+    DEFAULT_N_STEPS = 2048
+    DEFAULT_BUFFER_SIZE = 1000000
+    num_threads = int(psutil.cpu_count() / psutil.cpu_count(logical=False))
+    torch.set_num_threads(num_threads)
+    set_random_seed(seed)
+
+    hyperparams_data = read_hyperparams_data(yaml_path)
+    n_envs, policy, hyperparams, normalize, n_timesteps = parse_hyperparams(
+        env_name, hyperparams_data
+    )  # TODO : change batch_size with batch_factor
+    if "PPO" in mode:
+        if "n_steps" in hyperparams.keys():
+            hyperparams["n_steps"] = int(n_steps_factor * hyperparams["n_steps"])
+        else:
+            hyperparams["n_steps"] = int(DEFAULT_N_STEPS * n_steps_factor)
+    elif "SAC" in mode:
+        if "buffer_size" in hyperparams.keys():
+            hyperparams["buffer_size"] = int(n_steps_factor * hyperparams["buffer_size"])
+        else:
+            hyperparams["buffer_size"] = int(DEFAULT_BUFFER_SIZE * n_steps_factor)
+
+    if "policy_kwargs" in hyperparams.keys():
+        policy_kwargs = hyperparams["policy_kwargs"]
+        if "net_arch" in policy_kwargs.keys():
+            net_arch = policy_kwargs["net_arch"]
+            if "PPO" in mode:
+                net_arch["vf"] = [int(x * network_size_factor) for x in net_arch["vf"]]
+            else:
+                net_arch["qf"] = [int(x * network_size_factor) for x in net_arch["qf"]]
+            policy_kwargs["net_arch"] = net_arch
+        hyperparams["policy_kwargs"] = policy_kwargs
+    else:
+        if "PPO" in mode:
+            hyperparams["policy_kwargs"] = dict(
+                log_std_init=0,
+                ortho_init=True,
+                activation_fn=nn.Tanh,
+                net_arch=dict(pi=[64, 64], vf=[int(64 * network_size_factor), int(64 * network_size_factor)]),
+            )
+        else:  # SAC
+            hyperparams["policy_kwargs"] = dict(
+                net_arch=dict(pi=[256, 256], qf=[int(256 * network_size_factor), int(256 * network_size_factor)]),
+                activation_fn=nn.ReLU,
+            )
+
+    if flag == 1:
+        run_id = run.id
+    env = make_vec_env(env_name, n_envs=n_envs)
+    if normalize:
+        env = VecNormalize(env, gamma=hyperparams["gamma"] if "gamma" in hyperparams.keys() else 0.99)
+    if mode == "PPO":
+        agent = PPO
+    elif (mode == "AVEC_PPO") or (mode == "CORRECTED_AVEC_PPO"):
+        hyperparams["env_name"] = env_name
+        hyperparams["alpha"] = alpha
+        hyperparams["n_eval_timesteps"] = n_eval_timesteps
+        hyperparams["n_samples_MC"] = n_samples_MC
+        hyperparams["n_eval_envs"] = n_eval_envs
+        if mode == "AVEC_PPO":
+            agent = AVEC_PPO
+        elif mode == "CORRECTED_AVEC_PPO":
+            hyperparams["correction"] = True
+            agent = AVEC_PPO
+    elif mode == "SAC":
+        agent = SAC
+    elif "AVEC_SAC" in mode:  # or (mode == "CORRECTED_AVEC_PPO"):
+        hyperparams["env_name"] = env_name
+        hyperparams["alpha"] = alpha
+        hyperparams["n_eval_timesteps"] = n_eval_timesteps
+        hyperparams["n_samples_MC"] = n_samples_MC
+        hyperparams["n_eval_envs"] = n_eval_envs
+        if mode == "AVEC_SAC":
+            agent = AVEC_SAC
+        elif mode == "CORRECTED_AVEC_SAC":
+            hyperparams["correction"] = True
+            agent = AVEC_SAC
+    model = agent(
+        policy,
+        env,
+        tensorboard_log=f"runs/{run.id}",
+        **hyperparams,
+        seed=seed,
+    )
+    true_n_timesteps = n_timesteps if n_timesteps is not None else n_timesteps_user
+    # true_n_timesteps = int(1e4)
+    # Save a checkpoint every 1000 steps
+    folder = "./models"
+    os.makedirs(folder, exist_ok=True)
+    number_of_flags = 10
+    n_steps = model.n_steps if "PPO" in mode else model.train_freq.frequency
+    save_freq = ceil((true_n_timesteps / n_steps) * (1 / number_of_flags)) * n_steps
+    target_folder = os.path.join("/mnt/nfs_disk/yberthel/data", env_name, mode)
+    adjust_const = 1 if true_n_timesteps % n_steps != 0 else 0
+    upper_bound = int(((true_n_timesteps // n_steps) + adjust_const) * n_steps)
+    timesteps = min(upper_bound, int(save_freq * flag))
+    filename = f"{env_name}_{mode}_{alpha}_{seed}_{timesteps}"
+    copy_from_host(
+        os.path.join(folder, filename + ".zip"),
+        "flanders.gw",
+        os.path.join(target_folder, "models", filename + ".zip"),
+    )
+    assert os.path.exists(os.path.join(folder, filename + ".zip")), f"download failed for {filename}"
+
+    states_filename = f"states_{env_name}_{mode}_{alpha}_{seed}_{timesteps}"
+    copy_from_host(
+        os.path.join(folder, states_filename + ".pkl"),
+        "flanders.gw",
+        os.path.join(target_folder, "states", states_filename + ".pkl"),
+    )
+    assert os.path.exists(os.path.join(folder, states_filename + ".pkl")), f"download failed for {states_filename}"
+    states = read_from_pickle(os.path.join(folder, states_filename))
+    os.remove(os.path.join(folder, states_filename + ".pkl"))
+    if "SAC" in mode:
+        buffer_size = model.buffer_size
+    # old_alt_params = deepcopy(model.get_parameters()["policy"]["alternate_critic.qf0.0.weight"])
+    model = model.load(os.path.join(folder, filename))
+    # assert not torch.equal(
+    #     old_alt_params, model.get_parameters()["policy"]["alternate_critic.qf0.0.weight"]
+    # ), "alt critic didn't change after loading"
+    model.set_env(env)
+    # model._setup_learn(
+    #     0,
+    #     WandbCallback(),
+    #     False,
+    #     "run",
+    #     False,
+
+    # )
+    model._logger = utils.configure_logger(0, model.tensorboard_log, "run", False)
+    if "SAC" in mode:
+        number_of_files_needed = ceil(n_steps / model.replay_buffer.buffer_size)
+        buffer_files = []
+        for flag_idx in range(1, number_of_files_needed + 1):
+            flag_val = (1 / number_of_files_needed) * number_of_flags
+            closest_flag_val = find_nearest(list(range(1, number_of_flags + 1)), flag_val)
+            buffer_filename = f"replay_buffer_{env_name}_{mode}_{alpha}_{seed}_{closest_flag_val*save_freq}"
+            # artifact = wandb.use_artifact(f"{buffer_filename}:latest")
+            # datadir = artifact.download()
+            if not os.path.exists(os.path.join(folder, buffer_filename + ".pkl")):
+                copy_from_host(
+                    os.path.join(folder, buffer_filename + ".pkl"),
+                    "flanders.gw",
+                    os.path.join(target_folder, "replay_buffers", buffer_filename + ".pkl"),
+                )
+            assert os.path.exists(os.path.join(folder, buffer_filename + ".pkl")), f"download failed for {buffer_filename}"
+            buffer_files.append(buffer_filename)
+
+        number_of_files_needed = ceil(flag * save_freq / model.replay_buffer.buffer_size)
+        for i in range(number_of_files_needed):
+            buffer_filename = buffer_files[i]
+            temp_model = agent.load(os.path.join(folder, filename))
+            temp_model.load_replay_buffer(os.path.join(folder, buffer_filename))
+            # os.remove(os.path.join(folder, buffer_filename + ".pkl"))
+            lower_idx = max(0, flag * save_freq - buffer_size * (i + 1))
+            uppder_idx = min(flag * save_freq, buffer_size * (i + 1))
+            for values in ["next_observations", "actions", "observations", "rewards", "dones", "timeouts"]:
+                model.replay_buffer.__dict__[values][lower_idx:uppder_idx] = temp_model.replay_buffer.__dict__[values][
+                    lower_idx:uppder_idx
+                ]
+            del temp_model
+
+    os.remove(os.path.join(folder, filename + ".zip"))
+    if len(os.listdir(folder)) == 0:
+        shutil.rmtree(folder)
+    for param, value in hyperparams.items():
+        model.__dict__[param] = value
+    model.__dict__["n_eval_rollout_steps"] = n_eval_timesteps
+    model.__dict__["n_eval_rollout_envs"] = n_eval_envs
+    return model, states, number_of_flags, save_freq, env, folder, buffer_filename, target_folder

stable_baselines3/common/off_policy_algorithm.py
@@ -1602,6 +1602,7 @@ class AvecOffPolicyAlgorithm(BaseAlgorithm):
         n_iterations: int = 10,
         alternate_alpha: float = 0.5,
         n_rollout_timesteps: float = int(1e6),
+        replay_buffer=None,
     ) -> RolloutReturn:
         """
         Collect experiences and store them into a ``ReplayBuffer``.
@@ -1625,25 +1626,30 @@ class AvecOffPolicyAlgorithm(BaseAlgorithm):
         # Switch to eval mode (this affects batch norm / dropout)
         self.policy.set_training_mode(False)
         self.n_eval_rollout_steps = n_rollout_timesteps
+        print("Computing true_grads")
+        self.replay_buffer = deepcopy(replay_buffer)
         true_grads = compute_or_load_true_grads(
             self,
             n_flags,
             number_of_flags,
             alpha=alpha,
         )
-
+        self.replay_buffer = deepcopy(replay_buffer)
         alternate_true_grads = compute_or_load_true_grads(
             self,
             n_flags,
             number_of_flags,
             alpha=alternate_alpha,
         )
+        print("Finished Computing true_grads")
         pairwise_similarities, alternate_pairwise_similarities = [], []
         grads_list, alternate_grads_list = [], []
-
-        for num_rollout in range(1, n_iterations + 1):
+        print("Computing consecutive grads")
+        for num_rollout in tqdm(range(1, n_iterations + 1)):
             update = num_rollout == n_iterations
-            self.env.reset()
+            self._last_obs = self.env.reset()
+            self._last_episode_starts = np.ones((self.env.num_envs,), dtype=bool)
+            self.replay_buffer = deepcopy(replay_buffer)
 
             rollout = self.collect_rollouts(
                 self.env,